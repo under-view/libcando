57b07c8b8b803f9df63ed378defc2ce7cd0da55c
Vincent Davis Jr <vince@underview.tech>
2023-12-16 20:41:11 -0500

    make libhandy header only
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- include/log.h
+++ include/log.h
@@ -1,8 +1,20 @@
 #ifndef HANDY_LOG_H
 #define HANDY_LOG_H
 
+#include <stdlib.h>
 #include <stdio.h>
 #include <libgen.h>
+#include <stdint.h>
+#include <stdarg.h>
+#define HAVE_POSIX_TIMER
+#include <time.h>
+#ifdef CLOCK_MONOTONIC
+# define CLOCKID CLOCK_MONOTONIC
+#else
+# define CLOCKID CLOCK_REALTIME
+#endif
+
+#include "macros.h"
 
 /*
  * enum handy_log_level_type (Handy Log Level Type)
@@ -30,6 +42,9 @@ enum handy_log_level_type {
 };
 
 
+enum handy_log_level_type logLevel = HANDY_LOG_ALL;
+
+
 /*
  * handy_log_level_set: Sets which type of messages that are allowed
  *                      to be printed to an open file stream.
@@ -38,8 +53,22 @@ enum handy_log_level_type {
  * @level - 32-bit integer representing the type of log to print to
  *          an open file stream. Each log type has a different color.
  */
-void
-handy_log_level_set (enum handy_log_level_type level);
+HANDY_STATIC_INLINE void
+handy_log_level_set (enum handy_log_level_type level)
+{
+	logLevel = level;
+}
+
+
+/* ANSI Escape Codes */
+static const char *term_colors[] = {
+	[HANDY_LOG_NONE]    = "",
+	[HANDY_LOG_SUCCESS] = "\e[32;1m",
+	[HANDY_LOG_DANGER]  = "\e[31;1m",
+	[HANDY_LOG_INFO]    = "\e[35;1m",
+	[HANDY_LOG_WARNING] = "\e[33;1m",
+	[HANDY_LOG_RESET]   = "\x1b[0m"
+};
 
 
 /*
@@ -53,7 +82,35 @@ handy_log_level_set (enum handy_log_level_type level);
  * @...    - Variable list arguments
  */
 void
-handy_log (enum handy_log_level_type type, FILE *stream, const char *fmt, ...);
+handy_log (enum handy_log_level_type type, FILE *stream, const char *fmt, ...)
+{
+	va_list args;
+	time_t rawtime;
+	char buffer[26];
+
+	if (!(type & logLevel))
+		return;
+
+	/* create message time stamp */
+	rawtime = time(NULL);
+
+	/* generate time */
+	strftime(buffer, sizeof(buffer), "%F %T - ", localtime_r(&rawtime, &(struct tm){}));
+	fprintf(stream, "%s", buffer);
+
+	/* Set terminal color */
+	fprintf(stream, "%s", term_colors[type]);
+
+	va_start(args, fmt);
+	vfprintf(stream, fmt, args);
+	va_end(args);
+
+	/* Reset terminal colors */
+	fprintf(stream, "%s", term_colors[HANDY_LOG_RESET]);
+
+	/* Flush buffer */
+	fprintf(stream, "\n");
+}
 
 
 /*
@@ -62,5 +119,8 @@ handy_log (enum handy_log_level_type type, FILE *stream, const char *fmt, ...);
  */
 #define handy_logme(logType, fmt, ...) \
 	handy_log(logType, stdout, "[%s:%s:%d] " fmt, basename(__FILE__), __func__, __LINE__, ##__VA_ARGS__)
+
+#define handy_logme_err(fmt, ...) \
+	handy_log(HANDY_LOG_DANGER, stderr, "[%s:%s:%d] " fmt, basename(__FILE__), __func__, __LINE__, ##__VA_ARGS__)
 
 #endif /* HANDY_LOG_H */
