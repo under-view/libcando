1c80985ae2b05cfeea22b3603dd4f68b88cd5cee
Vincent Davis Jr <vince@underview.tech>
2024-12-07 15:58:10 -0500

    macros: add new macros CANDO_{SET,GET}_PAGE_WRITE
    Sets a given group of pages as writeable or readable.
    
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- include/macros.h
+++ include/macros.h
@@ -1,6 +1,9 @@
 #ifndef CANDO_MACROS_H
 #define CANDO_MACROS_H
 
+#include <stdint.h>   /* For uintptr_t */
+#include <sys/mman.h> /* For mprotect(2) */
+
 /* Prevent c++ name mangling */
 #ifdef __cplusplus
 #define CANDO_API extern "C"
@@ -36,5 +39,37 @@
  */
 #define CANDO_GET_PAGE(ptr) \
 	((void*)(((uintptr_t)ptr/CANDO_PAGE_SIZE)*CANDO_PAGE_SIZE))
+
+
+/*
+ * @brief Sets a grouping a pages write-only
+ *
+ * @param ptr  - Pointer to data caller wants write-only
+ * @param size - Size of data that needs to be set write-only
+ */
+#define CANDO_SET_PAGE_WRITE(ptr, size) \
+	__extension__ \
+	({ \
+		int err = -1; \
+		void *page = CANDO_GET_PAGE(ptr); \
+		err = mprotect(page, size, PROT_WRITE); \
+		err; \
+	})
+
+
+/*
+ * @brief Sets a grouping a pages read-only
+ *
+ * @param ptr  - Pointer to data caller wants write-only
+ * @param size - Size of data that needs to be set write-only
+ */
+#define CANDO_SET_PAGE_READ(ptr, size) \
+	__extension__ \
+	({ \
+		int err = -1; \
+		void *page = CANDO_GET_PAGE(ptr); \
+		err = mprotect(page, size, PROT_READ); \
+		err; \
+	})
 
 #endif /* CANDO_MACROS_H */
