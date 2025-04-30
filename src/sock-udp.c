d015c0bb961945c956500964c4c6248832ac9c7c
Vincent Davis Jr <vince@underview.tech>
2025-04-30 18:54:10 -0400

    sock-udp: initial implementation supports both ipv6 and ipv4
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ src/sock-udp.c
@@ -0,0 +1,468 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include "log.h"
+#include "macros.h"
+
+#include "sock-udp.h"
+
+/*
+ * @brief Structure defining Cando Socket UDP interface implementation.
+ *
+ * @member err     - Stores information about the error that occured
+ *                   for the given instance and may later be retrieved
+ *                   by caller.
+ * @member free    - If structure allocated with calloc(3) member will be
+ *                   set to true so that, we know to call free(3) when
+ *                   destroying the instance.
+ * @member fd      - File descriptor to the open VM socket.
+ * @member ip_addr - Ip address to sendto(2)/recvfrom(2).
+ * @member port    - UDP port number to sendto(2)/recvfrom(2).
+ * @member addr    - Stores IPV6 network byte information about the socket context.
+ *                   Is used for client connect(2) and server bind(2)/connect(2).
+ */
+struct cando_sock_udp
+{
+	struct cando_log_error_struct err;
+	bool                          free;
+	int                           fd;
+	char                          ip_addr[INET6_ADDRSTRLEN];
+	int                           port;
+	struct sockaddr_in6           addr;
+};
+
+
+/*****************************************
+ * Start of global to C source functions *
+ *****************************************/
+
+static struct cando_sock_udp *
+p_create_sock (struct cando_sock_udp *p_sock,
+               const void *p_sock_info)
+{
+	int err = -1;
+
+	struct cando_sock_udp *sock = p_sock;
+
+	const struct cando_sock_udp_create_info {
+		const char    *ip_addr;
+		int           port;
+		unsigned char ipv6 : 1;
+	} *sock_info = p_sock_info;
+
+	if (!sock_info || \
+	    !(sock_info->ip_addr))
+	{
+		cando_log_error("Incorrect data passed\n");
+		return NULL;
+	}
+
+	if (!sock) {
+		sock = calloc(1, sizeof(struct cando_sock_udp));
+		if (!sock) {
+			cando_log_error("calloc: %s\n", strerror(errno));
+			return NULL;
+		}
+
+		sock->free = true;
+	}
+
+	sock->fd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock->fd == -1) {
+		cando_log_error("socket(%u): %s\n", errno, strerror(errno));
+		cando_sock_udp_destroy(sock);
+		return NULL;
+	}
+
+	sock->port = sock_info->port;
+	snprintf(sock->ip_addr, sizeof(sock->ip_addr), "%s%s",
+	         (sock_info->ipv6) ? "" : "::ffff:", sock_info->ip_addr);
+
+	sock->addr.sin6_family = AF_INET6;
+	sock->addr.sin6_port = htons(sock_info->port);
+	err = inet_pton(AF_INET6, sock->ip_addr, &(sock->addr.sin6_addr));
+	if (err == 0) {
+		cando_log_error("'%s' invalid\n", sock->ip_addr);
+		cando_sock_udp_destroy(sock);
+		return NULL;
+	} else if (err == -1) {
+		cando_log_error("inet_pton: %s\n", strerror(errno));
+		cando_sock_udp_destroy(sock);
+		return NULL;
+	}
+
+	return sock;
+}
+
+/***************************************
+ * End of global to C source functions *
+ ***************************************/
+
+
+/********************************************
+ * Start of cando_sock_udp_server functions *
+ ********************************************/
+
+struct cando_sock_udp *
+cando_sock_udp_server_create (struct cando_sock_udp *p_sock,
+                              const void *p_sock_info)
+{
+	int err = -1;
+
+	const int enable = 1, disable = 0;
+
+	struct cando_sock_udp *sock = p_sock;
+
+	const struct cando_sock_udp_server_create_info *sock_info = p_sock_info;
+
+	sock = p_create_sock(sock, sock_info);
+	if (!sock)
+		return NULL;
+
+	err = setsockopt(sock->fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+	if (err == -1) {
+		cando_sock_udp_destroy(sock);
+		cando_log_error("setsockopt: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	err = setsockopt(sock->fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int));
+	if (err == -1) {
+		cando_sock_udp_destroy(sock);
+		cando_log_error("setsockopt: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	err = setsockopt(sock->fd, IPPROTO_IPV6, IPV6_V6ONLY,
+		(sock_info->ipv6) ? &enable : &disable, sizeof(int));
+	if (err == -1) {
+		cando_sock_udp_destroy(sock);
+		cando_log_error("setsockopt: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	err = bind(sock->fd, (const struct sockaddr*)&(sock->addr),
+			sizeof(struct sockaddr_in6));
+	if (err == -1) {
+		cando_sock_udp_destroy(sock);
+		cando_log_error("bind: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	return sock;
+}
+
+
+int
+cando_sock_udp_server_accept (struct cando_sock_udp *sock,
+                              const struct sockaddr_in6 *addr,
+                              const unsigned char ipv6)
+{
+	const char *ip_addr = NULL;
+
+	char buff[INET6_ADDRSTRLEN];
+
+	const int enable = 1, disable = 0;
+
+	int err = -1, sock_fd = -1, af = AF_INET6;
+
+	socklen_t len = sizeof(struct sockaddr_in6);
+
+	if (!sock)
+		return -1;
+
+	if (!addr) {
+		cando_log_set_error(sock, CANDO_LOG_ERR_INCORRECT_DATA, "");
+		return -1;
+	}
+
+	sock_fd = socket(af, SOCK_DGRAM, 0);
+	if (sock_fd == -1) {
+		cando_log_set_error(sock, errno, "socket: %s", strerror(errno));
+		return -1;
+	}
+
+	err = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "setsockopt: %s", strerror(errno));
+		close(sock_fd);
+		return -1;
+	}
+
+	err = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int));
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "setsockopt: %s", strerror(errno));
+		close(sock_fd);
+		return -1;
+	}
+
+	err = setsockopt(sock_fd, IPPROTO_IPV6, IPV6_V6ONLY,
+		(ipv6) ? &enable : &disable, sizeof(int));
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "setsockopt: %s", strerror(errno));
+		close(sock_fd);
+		return -1;
+	}
+
+	/*
+	 * Will temporary take over receiving from all,
+	 * but released after call to connect(2).
+	 */
+	err = bind(sock_fd, (const struct sockaddr*)addr, len);
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "bind: %s", strerror(errno));
+		close(sock_fd);
+		return -1;
+	}
+
+	err = connect(sock_fd, (const struct sockaddr*)addr, len);
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "connect: %s", strerror(errno));
+		close(sock_fd);
+		return -1;
+	}
+
+	ip_addr = inet_ntop(af, addr, buff, len);
+	cando_log(CANDO_LOG_INFO,
+	          "[+] Connected client fd '%d' at '%s:%u'\n",
+	          sock_fd, ip_addr, ntohs(addr->sin6_port));
+
+	return sock_fd;
+}
+
+
+ssize_t
+cando_sock_udp_server_recv_data (struct cando_sock_udp *sock,
+                                  void *data,
+                                  const size_t size,
+                                  struct sockaddr_in6 *addr,
+                                  const void *opts)
+{
+	if (!sock)
+		return -1;
+
+	return cando_sock_udp_recv_data(sock->fd, data,
+	                                size, addr, opts);
+}
+
+/******************************************
+ * End of cando_sock_udp_server functions *
+ ******************************************/
+
+
+/********************************************
+ * Start of cando_sock_udp_client functions *
+ ********************************************/
+
+struct cando_sock_udp *
+cando_sock_udp_client_create (struct cando_sock_udp *p_sock,
+                              const void *sock_info)
+{
+	struct cando_sock_udp *sock = p_sock;
+
+	sock = p_create_sock(sock, sock_info);
+	if (!sock)
+		return NULL;
+
+	return sock;
+}
+
+
+int
+cando_sock_udp_client_connect (struct cando_sock_udp *sock)
+{
+	int err = -1;
+
+	if (!sock)
+		return -1;
+
+	if (sock->fd <= 0) {
+		cando_log_set_error(sock, CANDO_LOG_ERR_INCORRECT_DATA, "");
+		return -1;
+	}
+
+	err = connect(sock->fd, (struct sockaddr*)&(sock->addr),
+			sizeof(struct sockaddr_in6));
+	if (err == -1) {
+		cando_log_set_error(sock, errno, "connect: %s", strerror(errno));
+		return -1;
+	}
+
+	cando_log(CANDO_LOG_SUCCESS,
+	          "[+] Connected to <ip_addr:port> '%s:%d'\n",
+	          sock->ip_addr, sock->port);
+
+	return 0;
+}
+
+
+ssize_t
+cando_sock_udp_client_send_data (struct cando_sock_udp *sock,
+                                 const void *data,
+                                 const size_t size,
+                                 const void *opts)
+{
+	if (!sock)
+		return -1;
+
+	return cando_sock_udp_send_data(sock->fd, data, size,
+	                                &(sock->addr), opts);
+}
+
+/***u**************************************
+ * End of cando_sock_udp_client functions *
+ ******************************************/
+
+
+/*****************************************
+ * Start of cando_sock_udp_get functions *
+ *****************************************/
+
+int
+cando_sock_udp_get_fd (struct cando_sock_udp *sock)
+{
+	if (!sock)
+		return -1;
+
+	return sock->fd;
+}
+
+
+const char *
+cando_sock_udp_get_ip_addr (struct cando_sock_udp *sock)
+{
+	if (!sock || \
+	    !(*sock->ip_addr))
+	{
+		return NULL;
+	}
+
+	return sock->ip_addr;
+}
+
+
+int
+cando_sock_udp_get_port (struct cando_sock_udp *sock)
+{
+	if (!sock)
+		return -1;
+
+	return sock->port;
+}
+
+/***************************************
+ * End of cando_sock_udp_get functions *
+ ***************************************/
+
+
+/*********************************************
+ * Start of cando_sock_udp_destroy functions *
+ *********************************************/
+
+void
+cando_sock_udp_destroy (struct cando_sock_udp *sock)
+{
+	if (!sock)
+		return;
+
+	close(sock->fd);
+	sock->fd = -1;
+
+	if (sock->free)
+		free(sock);
+}
+
+/*******************************************
+ * End of cando_sock_udp_destroy functions *
+ *******************************************/
+
+
+/**************************************************
+ * Start of non struct cando_sock param functions *
+ **************************************************/
+
+int
+cando_sock_udp_get_sizeof (void)
+{
+	return sizeof(struct cando_sock_udp);
+}
+
+
+ssize_t
+cando_sock_udp_recv_data (const int sock_fd,
+                          void *data,
+                          const size_t size,
+                          struct sockaddr_in6 *addr,
+                          const void *opts)
+{
+	ssize_t ret = 0;
+
+	socklen_t len = sizeof(struct sockaddr_in6);
+
+	const int flags = (opts) ? *((const int*)opts) : 0;
+
+	if (sock_fd < 0 || \
+	    !data || \
+	    !size)
+	{
+		return -1;
+	}
+
+	ret = recvfrom(sock_fd, data, size, flags,
+	               (struct sockaddr*) addr, &len);
+	if (errno == EINTR || errno == EAGAIN) {
+		return -errno;
+	} else if (ret == -1) {
+		cando_log_error("recv: %s", strerror(errno));
+		return -1;
+	}
+
+	return ret;
+}
+
+
+ssize_t
+cando_sock_udp_send_data (const int sock_fd,
+                          const void *data,
+                          const size_t size,
+                          const struct sockaddr_in6 *addr,
+                          const void *opts)
+{
+	ssize_t ret = 0;
+
+	socklen_t len = sizeof(struct sockaddr_in6);
+
+	const int flags = (opts) ? *((const int*)opts) : 0;
+
+	if (sock_fd < 0 || \
+	    !data || \
+	    !size)
+	{
+		return -1;
+	}
+
+	ret = sendto(sock_fd, data, size, flags,
+	             (const struct sockaddr*) addr, len);
+	if (errno == EINTR || errno == EAGAIN) {
+		return -errno;
+	} else if (ret == -1) {
+		cando_log_error("recv: %s", strerror(errno));
+		return -1;
+	}
+
+	return ret;
+}
+
+/*************************************************
+ * End of non struct cando_sock param functions *
+ *************************************************/
---
+++ tests/test-sock-udp.c
@@ -0,0 +1,382 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * Required by cmocka
+ */
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "log.h"
+#include "sock-udp.h"
+
+/**************************************************
+ * Start of test_sock_udp_server_create functions *
+ **************************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_server_create (void CANDO_UNUSED **state)
+{
+	struct cando_sock_udp *server = NULL;
+
+	struct cando_sock_udp_server_create_info server_info;
+
+	cando_log_set_level(CANDO_LOG_ALL);
+
+	server_info.ip_addr = "127.0.0.1";
+	server_info.port = 7777;
+	server_info.ipv6 = 0;
+	server = cando_sock_udp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	cando_sock_udp_destroy(server);
+}
+
+/************************************************
+ * End of test_sock_udp_server_create functions *
+ ************************************************/
+
+
+/**************************************************
+ * Start of test_sock_udp_client_create functions *
+ **************************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_client_create (void CANDO_UNUSED **state)
+{
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "127.0.0.1";
+	client_info.port = 7777;
+	client_info.ipv6 = 0;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	cando_sock_udp_destroy(client);
+}
+
+/************************************************
+ * End of test_sock_udp_client_create functions *
+ ************************************************/
+
+
+/***************************************************
+ * Start of test_sock_udp_accept_connect functions *
+ ***************************************************/
+
+static void
+p_test_sock_udp_accept_connect_client (void)
+{
+	int err = -1;
+
+	const int accept = 0x44;
+
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "127.0.0.1";
+	client_info.port = 7777;
+	client_info.ipv6 = 0;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	err = cando_sock_udp_client_connect(client);
+	assert_int_equal(err, 0);
+
+	/* Connect client to server */
+	err = cando_sock_udp_client_send_data(client, &accept, sizeof(int), NULL);
+	assert_int_equal(err, sizeof(int));
+
+	cando_sock_udp_destroy(client);
+
+	exit(0);
+}
+
+
+static void CANDO_UNUSED
+test_sock_udp_accept_connect (void CANDO_UNUSED **state)
+{
+	pid_t pid;
+
+	struct sockaddr_in6 addr;
+
+	int client_sock = -1, err = -1, data;
+
+	struct cando_sock_udp *server = NULL;
+
+	struct cando_sock_udp_server_create_info server_info;
+
+	cando_log_set_level(CANDO_LOG_ALL);
+
+	server_info.ip_addr = "127.0.0.1";
+	server_info.port = 7777;
+	server_info.ipv6 = 0;
+	server = cando_sock_udp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	pid = fork();
+	if (pid == 0) {
+		p_test_sock_udp_accept_connect_client();
+	}
+
+	err = cando_sock_udp_server_recv_data(server, &data,
+					sizeof(int), &addr, NULL);
+	assert_int_equal(err, sizeof(int));
+	assert_int_equal(data, 0x44);
+
+	client_sock = cando_sock_udp_server_accept(server, &addr, 0);
+	assert_int_not_equal(client_sock, -1);
+
+	/*
+	 * Just so printing data doesn't extend past the test.
+	 */
+	usleep(2000);
+
+	waitpid(pid, NULL, -1);
+
+	close(client_sock);
+	cando_sock_udp_destroy(server);
+}
+
+/*************************************************
+ * End of test_sock_udp_accept_connect functions *
+ *************************************************/
+
+
+/**********************************************
+ * Start of test_sock_udp_send_recv functions *
+ **********************************************/
+
+static void
+p_test_sock_udp_send_recv_client (void)
+{
+	int err = -1;
+
+	char buffer[512];
+
+	ssize_t size = 0;
+
+	const int accept = 0x44;
+
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "::1";
+	client_info.port = 7777;
+	client_info.ipv6 = 1;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	err = cando_sock_udp_client_connect(client);
+	assert_int_equal(err, 0);
+
+	/* Connect client to server */
+	err = cando_sock_udp_client_send_data(client, &accept, sizeof(int), NULL);
+	assert_int_equal(err, sizeof(int));
+
+	memset(buffer, 'T', sizeof(buffer));
+	size = cando_sock_udp_client_send_data(client, buffer, sizeof(buffer), 0);
+	assert_int_equal(size, sizeof(buffer));
+
+	cando_sock_udp_destroy(client);
+
+	exit(0);
+}
+
+
+static void CANDO_UNUSED
+test_sock_udp_send_recv (void CANDO_UNUSED **state)
+{
+	pid_t pid;
+
+	struct sockaddr_in6 addr;
+
+	char buffer[512], buffer_two[512];
+
+	int client_sock = -1, err = -1, data;
+
+	struct cando_sock_udp *server = NULL;
+
+	struct cando_sock_udp_server_create_info server_info;
+
+	cando_log_set_level(CANDO_LOG_ALL);
+
+	server_info.ip_addr = "::1";
+	server_info.port = 7777;
+	server_info.ipv6 = 1;
+	server = cando_sock_udp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	pid = fork();
+	if (pid == 0) {
+		p_test_sock_udp_send_recv_client();
+	}
+
+	err = cando_sock_udp_server_recv_data(server, &data,
+					sizeof(int), &addr, NULL);
+	assert_int_equal(err, sizeof(int));
+	assert_int_equal(data, 0x44);
+
+	client_sock = cando_sock_udp_server_accept(server, &addr, 0);
+	assert_int_not_equal(client_sock, -1);
+
+	/*
+	 * Just so printing data doesn't extend past the test.
+	 */
+	usleep(2000);
+
+	memset(buffer, 'T', sizeof(buffer));
+	cando_sock_udp_recv_data(client_sock, buffer_two, sizeof(buffer_two), NULL, 0);
+	assert_memory_equal(buffer, buffer_two, sizeof(buffer));
+
+	waitpid(pid, NULL, -1);
+
+	close(client_sock);
+	cando_sock_udp_destroy(server);
+}
+
+/********************************************
+ * End of test_sock_udp_send_recv functions *
+ ********************************************/
+
+
+/*******************************************
+ * Start of test_sock_udp_get_fd functions *
+ *******************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_get_fd (void CANDO_UNUSED **state)
+{
+	int sock_fd = -1;
+
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "127.0.0.1";
+	client_info.port = 7777;
+	client_info.ipv6 = 0;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	sock_fd = cando_sock_udp_get_fd(NULL);
+	assert_int_equal(sock_fd, -1);
+
+	sock_fd = cando_sock_udp_get_fd(client);
+	assert_int_not_equal(sock_fd, -1);
+
+	cando_sock_udp_destroy(client);
+}
+
+/*****************************************
+ * End of test_sock_udp_get_fd functions *
+ *****************************************/
+
+
+/************************************************
+ * Start of test_sock_udp_get_ip_addr functions *
+ ************************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_get_ip_addr (void CANDO_UNUSED **state)
+{
+	const char *ip_addr = NULL;
+
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "::1";
+	client_info.port = 7777;
+	client_info.ipv6 = 1;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	ip_addr = cando_sock_udp_get_ip_addr(NULL);
+	assert_null(ip_addr);
+
+	ip_addr = cando_sock_udp_get_ip_addr(client);
+	assert_string_equal(ip_addr, client_info.ip_addr);
+
+	cando_sock_udp_destroy(client);
+}
+
+/**********************************************
+ * End of test_sock_udp_get_ip_addr functions *
+ **********************************************/
+
+
+/*********************************************
+ * Start of test_sock_udp_get_port functions *
+ *********************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_get_port (void CANDO_UNUSED **state)
+{
+	int port = -1;
+
+	struct cando_sock_udp *client = NULL;
+
+	struct cando_sock_udp_client_create_info client_info;
+
+	client_info.ip_addr = "127.0.0.1";
+	client_info.port = 7777;
+	client_info.ipv6 = 0;
+	client = cando_sock_udp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	port = cando_sock_udp_get_port(NULL);
+	assert_int_equal(port, -1);
+
+	port = cando_sock_udp_get_port(client);
+	assert_int_equal(port, client_info.port);
+
+	cando_sock_udp_destroy(client);
+}
+
+/*******************************************
+ * End of test_sock_udp_get_port functions *
+ *******************************************/
+
+
+/***********************************************
+ * Start of test_sock_udp_get_sizeof functions *
+ ***********************************************/
+
+static void CANDO_UNUSED
+test_sock_udp_get_sizeof (void CANDO_UNUSED **state)
+{
+	int size = 0;
+	size = cando_sock_udp_get_sizeof();
+	assert_int_not_equal(size, 0);
+}
+
+/*********************************************
+ * End of test_sock_udp_get_sizeof functions *
+ *********************************************/
+
+int
+main (void)
+{
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_sock_udp_server_create),
+		cmocka_unit_test(test_sock_udp_client_create),
+		cmocka_unit_test(test_sock_udp_accept_connect),
+		//cmocka_unit_test(test_sock_udp_send_recv),
+		cmocka_unit_test(test_sock_udp_get_fd),
+		cmocka_unit_test(test_sock_udp_get_ip_addr),
+		cmocka_unit_test(test_sock_udp_get_port),
+		cmocka_unit_test(test_sock_udp_get_sizeof),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
