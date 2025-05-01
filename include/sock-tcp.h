87906c79470f76b8a180f3a6c14a930c4676c704
Vincent Davis Jr <vince@underview.tech>
2025-05-01 13:43:16 -0400

    sock-tcp: initial interface implementation
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

---
+++ include/sock-tcp.h
@@ -0,0 +1,267 @@
+#ifndef CANDO_SOCK_TCP_H
+#define CANDO_SOCK_TCP_H
+
+#include "macros.h"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/*
+ * Stores information about the cando_sock_tcp instace.
+ */
+struct cando_sock_tcp;
+
+
+/*
+ * @brief Structure passed to cando_sock_tcp_server_create(3)
+ *        used to define how to create the server.
+ *
+ * @member ipv6        - Boolean to determine if a socket is soley
+ *                       an ipv6 socket or requires a mapping from
+ *                       ipv4-to-ipv6.
+ * @member ip_addr     - Textual network IP addresses to accept(2) with.
+ * @member port        - Network port to accept(2) with.
+ * @member connections - Amount of connections that may be queued
+ *                       at a given moment.
+ */
+struct cando_sock_tcp_server_create_info
+{
+	unsigned char ipv6 : 1;
+	const char    *ip_addr;
+	int           port;
+	int           connections;
+};
+
+
+/*
+ * @brief Creates a socket that may be utilized for server socket operations.
+ *
+ * @param sock      - May be NULL or a pointer to a struct cando_sock_tcp.
+ *                    If NULL memory will be allocated and return to
+ *                    caller. If not NULL address passed will be used
+ *                    to store the newly created struct cando_sock_tcp
+ *                    instance.
+ * @param sock_info - Implementation uses a pointer to a
+ *                    struct cando_sock_tcp_server_create_info
+ *                    no other implementation may be passed to
+ *                    this parameter.
+ *
+ * @return
+ *	on success: Pointer to a struct cando_sock_tcp
+ *	on failure: NULL
+ */
+CANDO_API
+struct cando_sock_tcp *
+cando_sock_tcp_server_create (struct cando_sock_tcp *sock,
+                              const void *sock_info);
+
+
+/*
+ * @brief Accepts client connections returns file descriptor
+ *        to the connected client.
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ * @param addr - May be NULL or a pointer to a struct sockaddr_in6
+ *               If not NULL @addr is filled in via accept(2) call.
+ *
+ * @return
+ *	on success: File descriptor to accepted client
+ *	on failure: -1
+ */
+CANDO_API
+int
+cando_sock_tcp_server_accept (struct cando_sock_tcp *sock,
+                              struct sockaddr_in6 *addr);
+
+
+/*
+ * @brief Structure passed to cando_sock_tcp_client_create(3)
+ *        used to define how to create the server.
+ *
+ * @member ipv6    - Boolean to determine if a socket is soley
+ *                   an ipv6 socket or requires a mapping from
+ *                   ipv4-to-ipv6.
+ * @member ip_addr - Textual network IP addresses to accept(2) with.
+ * @member port    - Network port to connect(2)/send(2) to.
+ */
+struct cando_sock_tcp_client_create_info
+{
+	unsigned char ipv6 : 1;
+	const char    *ip_addr;
+	int           port;
+};
+
+
+/*
+ * @brief Creates a socket that may be utilized for client socket operations.
+ *
+ * @param sock      - May be NULL or a pointer to a struct cando_sock_tcp.
+ *                    If NULL memory will be allocated and return to
+ *                    caller. If not NULL address passed will be used
+ *                    to store the newly created struct cando_sock_tcp
+ *                    instance.
+ * @param sock_info - Implementation uses a pointer to a
+ *                    struct cando_sock_tcp_client_create_info
+ *                    no other implementation may be passed to
+ *                    this parameter.
+ *
+ * @return
+ *	on success: Pointer to a struct cando_sock_tcp
+ *	on failure: NULL
+ */
+CANDO_API
+struct cando_sock_tcp *
+cando_sock_tcp_client_create (struct cando_sock_tcp *sock,
+                              const void *sock_info);
+
+
+/*
+ * @brief Connects client socket to address provided via
+ *        call to cando_sock_tcp_client_create.
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ *
+ * @return
+ *	on success: 0
+ *	on failure: -1
+ */
+CANDO_API
+int
+cando_sock_tcp_client_connect (struct cando_sock_tcp *sock);
+
+
+/*
+ * @brief Send data to client socket address provided via
+ *        call to cando_sock_tcp_client_create(3).
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ * @param data - Pointer to data to send through socket.
+ * @param size - Size of data to send through socket.
+ * @param opts - Reserved for future usage. For now used
+ *               to set the flag argument of send(2).
+ *
+ * @return
+ *	on success: Amount of bytes sent
+ *	on failure: # < 0
+ */
+CANDO_API
+ssize_t
+cando_sock_tcp_client_send_data (struct cando_sock_tcp *sock,
+                                 const void *data,
+                                 const size_t size,
+                                 const void *opts);
+
+
+/*
+ * @brief Acquire socket file descriptor associated with
+ *        struct cando_sock_tcp instance.
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ *
+ * @return
+ *	on success: VM socket file descriptor
+ *	on failure: -1
+ */
+CANDO_API
+int
+cando_sock_tcp_get_fd (struct cando_sock_tcp *sock);
+
+
+/*
+ * @brief Acquire textual network address in string format
+ *        associated with struct cando_sock_tcp instance.
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ *
+ * @return
+ *	on success: Textual network address
+ *	on failure: NULL
+ */
+CANDO_API
+const char *
+cando_sock_tcp_get_ip_addr (struct cando_sock_tcp *sock);
+
+
+/*
+ * @brief Acquire network port associated with
+ *        struct cando_sock_tcp instance.
+ *
+ * @param sock - Must pass a pointer to a struct cando_sock_tcp.
+ *
+ * @return
+ *	on success: Network port connected to instance
+ *	on failure: -1
+ */
+CANDO_API
+int
+cando_sock_tcp_get_port (struct cando_sock_tcp *sock);
+
+
+/*
+ * @brief Frees any allocated memory and closes FD's (if open) created after
+ *        cando_sock_tcp_server_create() or cando_sock_tcp_client_create() call.
+ *
+ * @param sock - Pointer to a valid struct cando_sock_tcp.
+ */
+CANDO_API
+void
+cando_sock_tcp_destroy (struct cando_sock_tcp *sock);
+
+
+/*
+ * @brief Returns size of the internal structure. So,
+ *        if caller decides to allocate memory outside
+ *        of API interface they know the exact amount
+ *        of bytes.
+ *
+ * @return
+ *	on success: sizeof(struct cando_sock_tcp)
+ *	on failure: sizeof(struct cando_sock_tcp)
+ */
+CANDO_API
+int
+cando_sock_tcp_get_sizeof (void);
+
+
+/*
+ * @brief Receive data from socket file descriptor.
+ *
+ * @param sock_fd - Socket file descriptor to receive data from.
+ * @param data    - Pointer to data to store data received from a socket.
+ * @param size    - Size of data to receive from a socket.
+ * @param opts    - Reserved for future usage. For now used
+ *                  to set the flag argument of recv(2).
+ *
+ * @return
+ *	on success: Amount of bytes received
+ *	on failure: # < 0
+ */
+CANDO_API
+ssize_t
+cando_sock_tcp_recv_data (const int sock_fd,
+                          void *data,
+                          const size_t size,
+                          const void *opts);
+
+
+/*
+ * @brief Send data to socket file descriptor.
+ *
+ * @param sock_fd - Socket file descriptor to send data to.
+ * @param data    - Pointer to data to send through socket.
+ * @param size    - Size of data to send through socket.
+ * @param opts    - Reserved for future usage. For now used
+ *                  to set the flag argument of send(2).
+ *
+ * @return
+ *	on success: Amount of bytes sent
+ *	on failure: # < 0
+ */
+CANDO_API
+ssize_t
+cando_sock_tcp_send_data (const int sock_fd,
+                          const void *data,
+                          const size_t size,
+                          const void *opts);
+
+#endif /* CANDO_SOCK_TCP_H */
---
+++ tests/test-sock-tcp.c
@@ -0,0 +1,350 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/*
+ * Required by cmocka
+ */
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "log.h"
+#include "sock-tcp.h"
+
+/**************************************************
+ * Start of test_sock_tcp_server_create functions *
+ **************************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_server_create (void CANDO_UNUSED **state)
+{
+	struct cando_sock_tcp *server = NULL;
+
+	struct cando_sock_tcp_server_create_info server_info;
+
+	server_info.ipv6 = 0;
+	server_info.port = 7777;
+	server_info.ip_addr = "127.0.0.1";
+	server_info.connections = 1;
+	server = cando_sock_tcp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	cando_sock_tcp_destroy(server);
+}
+
+/************************************************
+ * End of test_sock_tcp_server_create functions *
+ ************************************************/
+
+
+/**************************************************
+ * Start of test_sock_tcp_client_create functions *
+ **************************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_client_create (void CANDO_UNUSED **state)
+{
+	struct cando_sock_tcp *client = NULL;
+
+	struct cando_sock_tcp_client_create_info client_info;
+
+	client_info.ipv6 = 0;
+	client_info.port = 7777;
+	client_info.ip_addr = "127.0.0.1";
+	client = cando_sock_tcp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	cando_sock_tcp_destroy(client);
+}
+
+/************************************************
+ * End of test_sock_tcp_client_create functions *
+ ************************************************/
+
+
+/***************************************************
+ * Start of test_sock_tcp_accept_connect functions *
+ ***************************************************/
+
+static void
+p_test_sock_tcp_accept_connect_client (void)
+{
+	int err = -1;
+
+	struct cando_sock_tcp *client = NULL;
+
+	struct cando_sock_tcp_client_create_info client_info;
+
+	client_info.ipv6 = 0;
+	client_info.port = 7777;
+	client_info.ip_addr = "127.0.0.1";
+	client = cando_sock_tcp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	err = cando_sock_tcp_client_connect(client);
+	assert_int_equal(err, 0);
+
+	cando_sock_tcp_destroy(client);
+
+	exit(0);
+}
+
+
+static void CANDO_UNUSED
+test_sock_tcp_accept_connect (void CANDO_UNUSED **state)
+{
+	pid_t pid;
+
+	int client_sock = -1;
+
+	struct cando_sock_tcp *server = NULL;
+
+	struct cando_sock_tcp_server_create_info server_info;
+
+	cando_log_set_level(CANDO_LOG_ALL);
+
+	server_info.ipv6 = 0;
+	server_info.port = 7777;
+	server_info.ip_addr = "127.0.0.1";
+	server_info.connections = 1;
+	server = cando_sock_tcp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	pid = fork();
+	if (pid == 0) {
+		p_test_sock_tcp_accept_connect_client();
+	}
+
+	client_sock = cando_sock_tcp_server_accept(server, NULL);
+	assert_int_not_equal(client_sock, -1);
+
+	waitpid(pid, NULL, -1);
+
+	close(client_sock);
+	cando_sock_tcp_destroy(server);
+}
+
+/*************************************************
+ * End of test_sock_tcp_accept_connect functions *
+ *************************************************/
+
+
+/**********************************************
+ * Start of test_sock_tcp_send_recv functions *
+ **********************************************/
+
+static void
+p_test_sock_tcp_send_recv_client (void)
+{
+	int err = -1;
+
+	char buffer[512];
+
+	ssize_t size = 0;
+
+	struct cando_sock_tcp *client = NULL;
+
+	struct cando_sock_tcp_client_create_info client_info;
+
+	client_info.ipv6 = 1;
+	client_info.port = 7777;
+	client_info.ip_addr = "::1";
+	client = cando_sock_tcp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	err = cando_sock_tcp_client_connect(client);
+	assert_int_equal(err, 0);
+
+	usleep(2000);
+
+	memset(buffer, 'T', sizeof(buffer));
+	size = cando_sock_tcp_client_send_data(client, buffer, sizeof(buffer), 0);
+	assert_int_equal(size, sizeof(buffer));
+
+	cando_sock_tcp_destroy(client);
+
+	exit(0);
+}
+
+
+static void CANDO_UNUSED
+test_sock_tcp_send_recv (void CANDO_UNUSED **state)
+{
+	pid_t pid;
+
+	int client_sock = -1;
+
+	char buffer[512], buffer_two[512];
+
+	struct cando_sock_tcp *server = NULL;
+
+	struct cando_sock_tcp_server_create_info server_info;
+
+	cando_log_set_level(CANDO_LOG_ALL);
+
+	server_info.ipv6 = 1;
+	server_info.port = 7777;
+	server_info.ip_addr = "::1";
+	server_info.connections = 1;
+	server = cando_sock_tcp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	pid = fork();
+	if (pid == 0) {
+		p_test_sock_tcp_send_recv_client();
+	}
+
+	client_sock = cando_sock_tcp_server_accept(server, NULL);
+	assert_int_not_equal(client_sock, -1);
+
+	memset(buffer, 'T', sizeof(buffer));
+	cando_sock_tcp_recv_data(client_sock, buffer_two, sizeof(buffer_two), 0);
+	assert_memory_equal(buffer, buffer_two, sizeof(buffer));
+
+	waitpid(pid, NULL, -1);
+
+	close(client_sock);
+	cando_sock_tcp_destroy(server);
+}
+
+/********************************************
+ * End of test_sock_tcp_send_recv functions *
+ ********************************************/
+
+
+/*******************************************
+ * Start of test_sock_tcp_get_fd functions *
+ *******************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_get_fd (void CANDO_UNUSED **state)
+{
+	int sock_fd = -1;
+
+	struct cando_sock_tcp *client = NULL;
+
+	struct cando_sock_tcp_client_create_info client_info;
+
+	client_info.ipv6 = 0;
+	client_info.port = 7777;
+	client_info.ip_addr = "127.0.0.1";
+	client = cando_sock_tcp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	sock_fd = cando_sock_tcp_get_fd(NULL);
+	assert_int_equal(sock_fd, -1);
+
+	sock_fd = cando_sock_tcp_get_fd(client);
+	assert_int_not_equal(sock_fd, -1);
+
+	cando_sock_tcp_destroy(client);
+}
+
+/*****************************************
+ * End of test_sock_tcp_get_fd functions *
+ *****************************************/
+
+
+/************************************************
+ * Start of test_sock_tcp_get_ip_addr functions *
+ ************************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_get_ip_addr (void CANDO_UNUSED **state)
+{
+	const char *ip_addr = NULL;
+
+	struct cando_sock_tcp *server = NULL;
+
+	struct cando_sock_tcp_server_create_info server_info;
+
+	server_info.ipv6 = 1;
+	server_info.port = 7777;
+	server_info.ip_addr = "::1";
+	server_info.connections = 1;
+	server = cando_sock_tcp_server_create(NULL, &server_info);
+	assert_non_null(server);
+
+	ip_addr = cando_sock_tcp_get_ip_addr(NULL);
+	assert_null(ip_addr);
+
+	ip_addr = cando_sock_tcp_get_ip_addr(server);
+	assert_string_equal(ip_addr, server_info.ip_addr);
+
+	cando_sock_tcp_destroy(server);
+}
+
+/**********************************************
+ * End of test_sock_tcp_get_ip_addr functions *
+ **********************************************/
+
+
+/*********************************************
+ * Start of test_sock_tcp_get_port functions *
+ *********************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_get_port (void CANDO_UNUSED **state)
+{
+	int port = -1;
+
+	struct cando_sock_tcp *client = NULL;
+
+	struct cando_sock_tcp_client_create_info client_info;
+
+	client_info.ipv6 = 0;
+	client_info.port = 7777;
+	client_info.ip_addr = "127.0.0.1";
+	client = cando_sock_tcp_client_create(NULL, &client_info);
+	assert_non_null(client);
+
+	port = cando_sock_tcp_get_port(NULL);
+	assert_int_equal(port, -1);
+
+	port = cando_sock_tcp_get_port(client);
+	assert_int_equal(port, client_info.port);
+
+	cando_sock_tcp_destroy(client);
+}
+
+/*******************************************
+ * End of test_sock_tcp_get_port functions *
+ *******************************************/
+
+
+/***********************************************
+ * Start of test_sock_tcp_get_sizeof functions *
+ ***********************************************/
+
+static void CANDO_UNUSED
+test_sock_tcp_get_sizeof (void CANDO_UNUSED **state)
+{
+	int size = 0;
+	size = cando_sock_tcp_get_sizeof();
+	assert_int_not_equal(size, 0);
+}
+
+/*********************************************
+ * End of test_sock_tcp_get_sizeof functions *
+ *********************************************/
+
+int
+main (void)
+{
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_sock_tcp_server_create),
+		cmocka_unit_test(test_sock_tcp_client_create),
+		cmocka_unit_test(test_sock_tcp_accept_connect),
+		cmocka_unit_test(test_sock_tcp_send_recv),
+		cmocka_unit_test(test_sock_tcp_get_fd),
+		cmocka_unit_test(test_sock_tcp_get_ip_addr),
+		cmocka_unit_test(test_sock_tcp_get_port),
+		cmocka_unit_test(test_sock_tcp_get_sizeof),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
