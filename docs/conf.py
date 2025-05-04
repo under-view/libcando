6e84cf461613cd404a93e9319c9cc28483935017
Vincent Davis Jr <vince@underview.tech>
2025-05-04 14:08:14 -0400

    release 0.1.7
    Signed-off-by: Vincent Davis Jr <vince@underview.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'libcando'
 copyright = '2024-2025, Vincent Davis Jr.'
 author = 'Vincent Davis Jr.'
-release = '0.1.6'
+release = '0.1.7'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
